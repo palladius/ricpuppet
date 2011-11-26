3bec407d309f762c7ffb564ac6057f8d65b616ca
Riccardo GitHubbi <rusko@palladius.it>
2011-11-26 12:02:12 +0000

    first tests

---
+++ helloworld/manifests/init.pp
@@ -0,0 +1,21 @@
+
+# Class: users
+#
+# This class installs our default set of users in our servers
+#
+# Parameters:
+#   $starting_uid:
+#       this global variable is used to set the minimum uid used for our users
+#
+# Actions:
+#   Install the default set of users: [dana,fox]
+#
+# Requires:
+#   - Package["zsh"]
+#
+# Sample Usage:
+#
+class users {
+              ...
+}
+
