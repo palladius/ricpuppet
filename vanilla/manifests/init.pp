2a6f714c9c5e08bc3c6d7cfe853d85204c7367cd
Riccardo GitHubbi <rusko@palladius.it>
2011-11-30 14:53:50 +0000

    help from DavidB

--- vanilla/manifests/init.pp
+++ vanilla/manifests/init.pp
@@ -1,6 +1,23 @@
 
+		# Gedeon 172.16.1.191
 class vanilla {
 
+
+	$username = 'ricpuppet'
+	$homedir = "/home/$username" 
+
+	user { $username:
+		ensure     => present ,
+		managename => true ,
+		shell      => '/bin/bash' ,
+		home       => $homedir,
+		require    => File[$homedir]
+
+	}
+
+	package { 'ssh':
+		ensure => present ,
+	}
 
 
 
