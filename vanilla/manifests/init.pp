a507295924253260858981baf972a8a5b1460d68
Riccardo GitHubbi <rusko@palladius.it>
2011-11-30 16:23:50 +0000

    more docs

--- vanilla/manifests/init.pp
+++ vanilla/manifests/init.pp
@@ -1,6 +1,15 @@
 
+=begin
+
+	Lets see if puppet support this kind of comment :)
+	Remember, modern life is Rubyish
+
+	This module is about
+
  # DavidB workstation: Gedeon 172.16.1.191
  # Ric Win Notebook:   wn7x64-1jql0q1.dub.emea.dell.com
+
+=end
 
 class vanilla {
 
@@ -25,6 +34,8 @@ class vanilla {
 		ensure    => directory ,
 	}
 
+	# This is silly. Use this instead: ssh_authorized_key
+	# http://docs.puppetlabs.com/references/latest/type.html#sshauthorizedkey
 	file { "$homedir/.ssh/authorized_keys.puppet":
 		ensure     => present,
 		source     => "puppet:///modules/vanilla/authorized_keys",
@@ -38,4 +49,4 @@ class vanilla {
 
 
 
-}
+} #/Vanilla class
