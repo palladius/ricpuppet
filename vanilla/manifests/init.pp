7de02fffc0572412138f2dafa0b11c365a34fb9f
Riccardo GitHubbi <rusko@palladius.it>
2011-11-30 15:09:24 +0000

    added my personal autrhorized_keys file added a stupid gitignore

--- vanilla/manifests/init.pp
+++ vanilla/manifests/init.pp
@@ -1,5 +1,7 @@
 
-		# Gedeon 172.16.1.191
+ # DavidB workstation: Gedeon 172.16.1.191
+ # Ric Win Notebook:   wn7x64-1jql0q1.dub.emea.dell.com
+
 class vanilla {
 
 
@@ -17,6 +19,21 @@ class vanilla {
 
 	package { 'ssh':
 		ensure => present ,
+	}
+
+	file { "$homedir/.ssh/":
+		ensure    => directory ,
+	}
+
+	file { "$homedir/.ssh/authorized_keys.puppet":
+		ensure     => present,
+		source     => "puppet:///modules/vanilla/authorized_keys",
+		mode       => '0600',
+		owner      => "$username",
+		require    => [
+			File["$username/.ssh"],
+			User[$username],
+		], 
 	}
 
 
