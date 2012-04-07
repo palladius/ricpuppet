ee73ec9cf8982bf77fb7233704279a0e2a66331a
Riccardo Carlesso <riccardo_carlesso@dell.com>
2012-04-07 09:59:59 +0200

    better docs and tabbing for puppet-lint

--- logrotate/manifests/init.pp
+++ logrotate/manifests/init.pp
@@ -1,20 +1,20 @@
 # ensure logrotate package is installed
 # ensure that /etc/logrotate.d exists
 # drop files in /etc/logrotate.d
-
-
+#
+# Note. Should only work on Ubuntu/Debian. Debug it on Redhat.
 class logrotate::base {
 
-        package { logrotate:
-                ensure => installed,
-        }
+  package { logrotate:
+    ensure => installed,
+  }
 
-        file { "/etc/logrotate.d":
-                ensure => directory,
-                owner => root,
-                group => root,
-                mode => 755,
-                require => Package[logrotate],
-        }
+  file { "/etc/logrotate.d":
+    ensure => directory,
+    owner => root,
+    group => root,
+    mode => 755,
+    require => Package[logrotate],
+  }
 
 }
