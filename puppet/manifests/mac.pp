193bd4daed9ce5a31371b6ec7270906049f92343
Riccardo Carlesso <riccardo_carlesso@dell.com>
2012-04-07 11:26:02 +0200

    to add puppet process to launchd (like /etc/init.d)
    Found here: http://projects.puppetlabs.com/projects/puppet/wiki/Puppet_With_Launchd

---
+++ puppet/manifests/mac.pp
@@ -0,0 +1,5 @@
+# TODO
+# if architecture is Mac then load this module.
+#
+# launchctl load -w /Library/LaunchDaemons/com.reductivelabs.puppet.plist
+# launchctl load -w /Library/LaunchDaemons/com.reductivelabs.puppetmaster.plist
