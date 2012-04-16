60c03f0c4cc123d8d21063ca8e42b8a6047ea52e
Riccardo Carlesso <riccardo_carlesso@dell.com>
2012-04-16 17:58:03 +0100

    18 -> 18a just to see where its updated!

--- sauce/manifests/init.pp
+++ sauce/manifests/init.pp
@@ -20,7 +20,7 @@ class sauce () {
   #include sauce::os       # Adds OS specific code! (Recafctorung Mac OS/X out of here!)
   #include vcsrepo
 
-  $version = '1.2.18'
+  $version = '1.2.18b'
   $verbose = true
   $basepath = '/opt/palladius'
   $default_poweruser_name  = 'riccardo'
