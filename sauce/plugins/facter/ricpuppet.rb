ab411128183943822454b373ad4396c44a704cdd
Riccardo Carlesso <riccardo_carlesso@dell.com>
2012-04-18 08:49:07 +0100

    adding version
    doesnt work yet but at least its stubbed.

---
+++ sauce/plugins/facter/ricpuppet.rb
@@ -0,0 +1,19 @@
+
+# locate git/ricpuppet/VERSION
+Facter.add("ricpuppet_version") do
+    setcode do
+        '4.5.6TODO'
+    end
+end
+
+Facter.add("sauce_version") do
+    setcode do
+        '1.2.3TODO'
+    end
+end
+
+Facter.add("pwd") do
+    setcode do
+        `pwd`
+    end
+end
